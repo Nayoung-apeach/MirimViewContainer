a627c552c45ee3e37d47c1926e7c4a3417027359
Nayoung-apeach <s2019w04@e-mirim.hs.kr>
2020-11-05 20:28:02 +0900

    Not ViewFlipper Listener :D

---
+++ app/src/main/java/kr/hs/emirim/s2019w04/mirimviewcontainer/TabActivityTest2.java
@@ -0,0 +1,30 @@
+package kr.hs.emirim.s2019w04.mirimviewcontainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class TabActivityTest2 extends TabActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_test2);
+
+        TabHost tabHost = getTabHost();
+        TabHost.TabSpec tabSpecPark = tabHost.newTabSpec("PARK").setIndicator("박정우");
+        tabSpecPark.setContent(R.id.linearPark);
+        tabHost.addTab(tabSpecPark);
+
+        TabHost.TabSpec tabSpecKim = tabHost.newTabSpec("KIM").setIndicator("김도영");
+        tabSpecKim.setContent(R.id.linearKim);
+        tabHost.addTab(tabSpecKim);
+
+        TabHost.TabSpec tabSpecHa = tabHost.newTabSpec("HA").setIndicator("하루토");
+        tabSpecHa.setContent(R.id.linearHa);
+        tabHost.addTab(tabSpecHa);
+        tabHost.setCurrentTab(0);
+    }
+}
